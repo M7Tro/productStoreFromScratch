5164c5fd6dfefe0a701a32b65d6892027f01d48e
M7Tr0_ <mechanozver@gmail.com>
2024-09-03 21:51:42 +0800

    set up chakra

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter as Router} from 'react-router-dom';
+import {ChakraProvider} from "@chakra-ui/react";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ChakraProvider>
+      <Router>
+        <App />
+      </Router>      
+    </ChakraProvider>
   </StrictMode>,
 )
