948d63875a5243c6dfc60b95ea8c14219fbac735
M7Tr0_ <mechanozver@gmail.com>
2024-09-03 22:02:22 +0800

    added a navbar using buttons and HStack. Actually, I think using Chakra is not that bad after all

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import {Link} from 'react-router-dom';
+import {Button, HStack} from "@chakra-ui/react";
 
 export default function Navbar () {
     return (
-        <nav>
-            <Link to={'/'}>HomePage</Link>
-            <Link to={'/create'}>Create</Link>
-        </nav>
+        <HStack as={'nav'}>
+            <Link to={'/'}>
+                <Button colorScheme='red' variant={'solid'}>Product Store</Button>
+            </Link>
+            <Link to={'/create'}>
+                <Button colorScheme='red' variant={'solid'}>Create</Button>
+            </Link>            
+        </HStack>
     )
 }
