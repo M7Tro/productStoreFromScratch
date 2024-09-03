175152a0c03895ddc4ad8c12fa7ce61a8931669c
M7Tr0_ <mechanozver@gmail.com>
2024-09-03 22:57:15 +0800

    implemented the navbar using the approach from tutorial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,16 @@ import {Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import Create from './pages/Create';
+import { Box } from '@chakra-ui/react';
 
 export default function App () {
   return (
-    <div>
+    <Box minH={"100vh"}>
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<HomePage/>}></Route>
         <Route path='/create' element={<Create/>}></Route>
-      </Routes>
-    </div>
+      </Routes>      
+    </Box>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import {Link} from 'react-router-dom';
-import {Button, HStack, useColorMode, Text, Box} from "@chakra-ui/react";
+import {Button, HStack, useColorMode, Text, Box, Container, Flex} from "@chakra-ui/react";
 import { FaRegPlusSquare } from "react-icons/fa";
 import { MdOutlineLightMode, MdDarkMode} from "react-icons/md";
 
@@ -7,27 +7,29 @@ import { MdOutlineLightMode, MdDarkMode} from "react-icons/md";
 export default function Navbar () {
     const {colorMode, toggleColorMode} = useColorMode();
     return (
-        <HStack as={'nav'} justify={"space-around"} p={4}>
-            <Link to={'/'}>
-                <Text
-                bgGradient='linear(to-l, #7928CA, #FF0080)'
-                bgClip='text'
-                fontSize='3xl'
-                fontWeight='extrabold'
-                >
-                    Welcome to Chakra UI
-                </Text>
-            </Link>
-            <Box>
-                <Link to={'/create'}>
-                    <Button colorScheme='purple' variant={'solid'} mr={4}>
-                        <FaRegPlusSquare/>
+        <Container maxW={"80vw"}>
+            <Flex my={10} direction={{base:"column", sm:"row"}}  justify={"space-between"}>
+                <Link to={'/'}>
+                    <Text
+                    bgGradient='linear(to-l, #7928CA, #FF0080)'
+                    bgClip='text'
+                    fontSize='4xl'
+                    fontWeight='extrabold'
+                    >
+                        Product Store
+                    </Text>
+                </Link>
+                <HStack>
+                    <Link to={'/create'}>
+                        <Button>
+                            <FaRegPlusSquare/>
+                        </Button>
+                    </Link>  
+                    <Button onClick={toggleColorMode}>
+                        {colorMode === "light" ? <MdOutlineLightMode/> : <MdDarkMode/>}
                     </Button>
-                </Link>            
-                <Button onClick={toggleColorMode}>
-                    {colorMode==='light' ? <MdOutlineLightMode/> : <MdDarkMode/>}
-                </Button>                
-            </Box>
-        </HStack>
+                </HStack>
+            </Flex>
+        </Container>
     )
 }
