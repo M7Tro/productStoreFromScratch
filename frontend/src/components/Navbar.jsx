3a134507dc8e3b1ffe71b1bae4f33eb96f1b5ca3
M7Tr0_ <mechanozver@gmail.com>
2024-09-03 22:04:57 +0800

    tried using the useColorMode hook to change the theme to dark or light. It is actually quite straightforward

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import {Link} from 'react-router-dom';
-import {Button, HStack} from "@chakra-ui/react";
+import {Button, HStack, useColorMode} from "@chakra-ui/react";
 
 export default function Navbar () {
+    const {toggleColorMode} = useColorMode();
     return (
         <HStack as={'nav'}>
             <Link to={'/'}>
@@ -10,6 +11,7 @@ export default function Navbar () {
             <Link to={'/create'}>
                 <Button colorScheme='red' variant={'solid'}>Create</Button>
             </Link>            
+            <Button onClick={toggleColorMode}>Change Color Mode</Button>
         </HStack>
     )
 }
