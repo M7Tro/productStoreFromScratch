f2d842b0a99bf7c8c4122f47febcaea43906adfa
M7Tr0_ <mechanozver@gmail.com>
2024-09-03 22:22:01 +0800

    implemented a navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,33 @@
 import {Link} from 'react-router-dom';
-import {Button, HStack, useColorMode} from "@chakra-ui/react";
+import {Button, HStack, useColorMode, Text, Box} from "@chakra-ui/react";
+import { FaRegPlusSquare } from "react-icons/fa";
+import { MdOutlineLightMode, MdDarkMode} from "react-icons/md";
+
 
 export default function Navbar () {
-    const {toggleColorMode} = useColorMode();
+    const {colorMode, toggleColorMode} = useColorMode();
     return (
-        <HStack as={'nav'}>
+        <HStack as={'nav'} justify={"space-around"} p={4}>
             <Link to={'/'}>
-                <Button colorScheme='red' variant={'solid'}>Product Store</Button>
+                <Text
+                bgGradient='linear(to-l, #7928CA, #FF0080)'
+                bgClip='text'
+                fontSize='3xl'
+                fontWeight='extrabold'
+                >
+                    Welcome to Chakra UI
+                </Text>
             </Link>
-            <Link to={'/create'}>
-                <Button colorScheme='red' variant={'solid'}>Create</Button>
-            </Link>            
-            <Button onClick={toggleColorMode}>Change Color Mode</Button>
+            <Box>
+                <Link to={'/create'}>
+                    <Button colorScheme='purple' variant={'solid'} mr={4}>
+                        <FaRegPlusSquare/>
+                    </Button>
+                </Link>            
+                <Button onClick={toggleColorMode}>
+                    {colorMode==='light' ? <MdOutlineLightMode/> : <MdDarkMode/>}
+                </Button>                
+            </Box>
         </HStack>
     )
 }
